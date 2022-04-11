625b2a5b0811caaac66285764366b20e793afac6
Abhi Kafle <akafle848@gmail.com>
2022-04-11 19:39:08 +0545

    First push

---
+++ pdf-link.php
@@ -0,0 +1,9 @@
+<?php 
+$directory = "content/yogi-narharinath-letters/";
+$phpfiles = scandir($directory, 1);
+
+foreach ($phpfiles as $php_file)
+{
+    echo '<li><a href = "'.$directory/$php_file.'">' .basename($php_file). '</a></li>>';
+}
+?>
